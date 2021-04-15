e498bc80a92816f7065081ba4c41a4b3d7ed7552
fierygreen <79273582+fierygreen@users.noreply.github.com>
2021-04-15 10:45:29 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+e e b f d d d d d d a a a a d d 
+e b b b b b b b a a 9 9 d d d d 
+e e a f d 8 8 b 8 d d 9 b b d d 
+e a 9 8 8 8 8 b b b b b d b d d 
+a a 9 f 2 8 3 3 a a a a d b 7 d 
+a 2 9 f 3 3 3 3 a a a 9 d b d d 
+a 2 9 f 6 3 3 3 3 8 a a 7 b d d 
+a a 9 5 f 6 3 2 6 2 8 a 6 b d d 
+a a 9 5 3 3 a 2 6 8 2 a a b a d 
+a a 9 5 3 3 3 2 8 9 9 2 a b d d 
+a a a a f 3 8 8 2 3 9 2 a b d d 
+6 a a 6 f 3 3 3 3 3 2 2 b b d d 
+a 6 9 6 f 6 2 2 2 2 2 9 6 a a d 
+a a a 6 f 6 2 2 8 2 6 9 2 e e a 
+8 a a 6 5 f 6 2 2 6 6 9 e e e e 
+8 8 9 9 9 9 9 9 9 9 9 9 e 6 e e 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,11 +1,33 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    music.stopAllSounds()
-    music.playMelody("E B C5 A B G A F ", 180)
-    music.playMelody("E - E E - F - D ", 180)
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+function music1 () {
     music.stopAllSounds()
     music.playMelody("G G E - A E F F ", 180)
     music.playMelody("D G G D E E C F ", 180)
     music.playMelody("A F D - C - C - ", 180)
-})
+}
+function music2 () {
+    music.stopAllSounds()
+    music.playMelody("E B C5 A B G A F ", 180)
+    music.playMelody("E - E E - F - D ", 180)
+}
+let mySprite = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f 4 4 4 4 4 4 4 2 2 2 2 2 2 2 f 
+    f 4 4 4 4 2 2 2 2 2 2 2 2 2 2 f 
+    f 4 4 4 4 2 2 2 2 2 2 2 2 2 2 f 
+    f 4 4 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 4 4 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 4 4 2 1 f 2 2 2 2 2 1 f 2 2 f 
+    f 4 2 2 2 2 2 f 2 f 2 2 2 2 2 f 
+    f 4 2 2 2 2 2 f f f 2 2 2 2 2 f 
+    f 4 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+    f 4 2 2 2 2 2 2 2 2 2 2 2 2 5 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 2 5 5 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 5 5 5 f 
+    f 2 2 2 2 2 2 2 2 2 2 2 5 5 5 f 
+    f 2 2 2 2 2 2 2 2 5 5 5 5 5 5 f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+while (0 == 0) {
+	
+}
